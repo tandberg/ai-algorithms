9a14c32c12454949e5400c3bd175ebe6890461a3
Øystein Tandberg <tandeey@gmail.com>
2014-05-08 12:41:40 +0200

    EM to take N argument

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
-AI algorithms
-==============
-All the algorithms is written as a standalone program in Python.
+# AI algorithms
+This is a repository including the implementation of some basic Artificial Intelligence algorithms. All the algorithms is written as a standalone program in Python.
+
+## Algorithms
+
 
 - Expectation Maximation
 - Simulated Annealing (comming)
 - A* search (comming)
 - Artificial Neural Network (comming)
 - Min-conflicts (comming)
-- Evolutionary algorithm (comming)
-    - Full population Replacement
-    - Over production
-    - Generation mixing
-    - Tournament section
-    - Boltzmann selection
-    - Sigma scaling
-    - Fitness Proportionate
 - Minimal Independent Set (comming)
+- K-means (comming)
+- Evolutionary algorithm (comming)
+	- Parent-mate selection
+    	- Fitness Proportionate
+    	- Sigma scaling
+    	- Tournament section
+    	- Boltzmann selection
+    - Adult selection
+    	- Full population Replacement
+    	- Over production
+    	- Generation mixing
+
+## Usage
+Run the algorithms from the command line typnig:
+
+```
+python [algorithm-file].py arguments
+```
+
+## Dependencies
+
+???
--- expectation_maximation.py
+++ expectation_maximation.py
@@ -1,59 +1,54 @@
 from random import random
-from math import exp, pow
+from copy import copy
+import math
+import sys
+
 
 class ExpectationMaximation(object):
-	def __init__(self, data):
-		self.data = data
-		self.em()
+	def __init__(self, data, hyps=1):
+		self.MAX_ITERATIONS = 1000
+		self.em(data, hyps)
 
-	def em(self): # TODO: pass inn number of hyps and do it generic
-		hyp1 = random()
-		hyp2 = random()
+	def em(self, instances, count): # TODO: Prettify python code
 
-		instances = []
+		hyps = [random()] * count
+		hypsOld = [-1] * count
 
-		for point in self.data:
-			instances.append(point)
+		def condition(hyps, oldHyps, accumulator):
+			if len(hyps) == 0:
+				return accumulator
+			return condition(hyps[1:], oldHyps[1:], accumulator and hyps[0] != oldHyps[0])
 
-		hyp1old = -1
-		hyp2old = -1
+		iterations = 0
+		while condition(hyps, hypsOld, True) or iterations > self.MAX_ITERATIONS:
+			iterations += 1
+			hypsOld = copy(hyps)
 
-		while(hyp1 != hyp1old and hyp2 != hyp2old):
-			hyp1old = hyp1
-			hyp2old = hyp2
+			for i in range(count):
+				sum1 = 0
+				sum2 = 0
 
-			sum1 = 0
-			sum2 = 0
+				for instance in instances:
+					sum1 += self.expectation(instance.x, hyps[i], hyps) * instance.x
+					sum2 += self.expectation(instance.x, hyps[i], hyps)
 
-			for instance in instances:
-				sum1 += self.expectation(instance.x, hyp1, hyp1, hyp2) * instance.x
-				sum2 += self.expectation(instance.x, hyp1, hyp1, hyp2)
+				hyps[i] = sum1 / sum2
 
-			hyp1 = sum1 / sum2
+		print '\nConverged at {0} iterations.\nResult: {1}'.format(iterations, hyps)
 
-			sum1 = 0
-			sum2 = 0
-
-			for instance in instances:
-				sum1 += self.expectation(instance.x, hyp2, hyp1, hyp2) * instance.x
-				sum2 += self.expectation(instance.x, hyp2, hyp1, hyp2)
-
-			hyp2 = sum1 / sum2
-
-		print 'Done\nHyp1: {0}, Hyp2: {1}'.format(hyp1, hyp2)
-
-	def expectation(self, data, mu, hyp1, hyp2):
-		return self.probability(data, mu) / (self.probability(data, hyp1) + self.probability(data, hyp2))
+	def expectation(self, data, mu, hyps):
+		denominator = sum(map(lambda x: self.probability(data, x), hyps))
+		return self.probability(data, mu) / denominator
 
 	def probability(self, x, m):
-		return exp(-0.5 * pow(x-m, 2))
+		return math.exp(-0.5 * math.pow(x-m, 2))
 
 
 class Instance(object):
 	def __init__(self, x):
 		self.x = x
 	def __repr__(self):
-		return 'instance-{0}'.format(self.x)
+		return 'Instance-{0}'.format(self.x)
 
 def main():
 	file = open('data/em_sample_data.txt', 'r')
@@ -64,7 +59,10 @@ def main():
 		data.append(Instance(float(line)))
 		line = file.readline()
 
-	ExpectationMaximation(data)
+	if len(sys.argv) > 1:
+		ExpectationMaximation(data, int(sys.argv[1]))
+	else:
+		ExpectationMaximation(data)
 
 if __name__ == '__main__':
 	main()
